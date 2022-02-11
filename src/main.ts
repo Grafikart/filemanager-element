96280e28a3941952c0f8dc8723a2f3bbc22de9e1
Jonathan <contact@grafikart.fr>
2022-02-11 19:12:24 +0100

    feat: debug event
    Signed-off-by: Jonathan <contact@grafikart.fr>

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,11 @@
 import { FileManager } from './FileManager'
 
 FileManager.define();
+
+document.querySelector('file-manager').addEventListener('close', () => {
+  console.log('close')
+})
+
+document.querySelector('file-manager').addEventListener('selectfile', (e: CustomEvent) => {
+  console.log('fileselect', e.detail)
+})
